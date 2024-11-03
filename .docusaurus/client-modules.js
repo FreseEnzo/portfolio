ac26183423de1f242cf078c9da153da6e8b1a1c7
Frese <enzo.frese@hardwario.com>
2024-11-03 20:51:04 -0300

    Deploy website - based on 3bc57dc1ded2c425763635e02c72be1bbec278ba

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/home/frese/portfolio/node_modules/infima/dist/css/default/default.css"),
-  require("/home/frese/portfolio/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/frese/portfolio/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/frese/portfolio/src/css/custom.css"),
-];
